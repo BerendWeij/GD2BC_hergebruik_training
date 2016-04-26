1ba38d97ed5461d72175b177764cc39d8d002161
BartGeest <kkcbart@live.nl>
2016-04-26 14:36:58 +0100

    first commit, added follow function and getter setter for target in base class

---
+++ Assets/Scripts/CameraBase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CameraBase : MonoBehaviour {
+
+    private GameObject _target;
+
+    //not in a vector 3 because you might want to adjust one axis
+    private float x_Offset;
+    private float y_Offset;
+    private float z_Offset;
+
+    //using LateUpdate because you want your target to move before following it
+    void LateUpdate()
+    {
+        FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        transform.position = new Vector3(_target.transform.position.x + x_Offset,
+                                         _target.transform.position.y + y_Offset,
+                                         _target.transform.position.z + z_Offset);
+    }
+
+    public GameObject SetTarget
+    {
+        get { return _target; }
+
+        set { _target = value; }
+    }
+}
---
+++ Assets/Scripts/CubeMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour {
+
+    private float _moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+    }
+}
