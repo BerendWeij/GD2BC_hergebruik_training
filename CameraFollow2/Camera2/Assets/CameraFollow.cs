1d298b14c4afd5cf05a35ebbb55df5e0a1c71490
Thomacia <thomas@thoemda.com>
2016-04-26 15:01:35 +0200

    Push
    ub

---
+++ CameraFollow2/Camera2/Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]private GameObject _tar;
+    [SerializeField]private GameObject _player;
+    [SerializeField]private int _followDistance;
+
+   // put this script on your camera and assign _tar
+    void Update ()
+    {
+        
+        FollowTar();
+        transform.LookAt(_player.transform);
+    }
+	
+
+    void FollowTar()
+    {
+        var TarPos = _tar.transform.position;
+        TarPos = new Vector3(TarPos.x, TarPos.y, TarPos.z + _followDistance);
+        transform.position = Vector3.Lerp(transform.position, TarPos, Time.deltaTime);
+    }
+}
