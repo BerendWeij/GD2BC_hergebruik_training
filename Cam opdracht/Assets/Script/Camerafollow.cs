680ce23c634f9dfa65f4785971050185bd655392
tommtom9 <tommtom9@live.nl>
2016-05-10 13:56:46 +0200

    Opdracht

---
+++ Cam opdracht/Assets/Script/Camerafollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Camerafollow : MonoBehaviour {
+
+    [SerializeField]
+    GameObject _followObject;
+
+    [SerializeField]
+    bool _followWithAnimation;
+    [SerializeField]
+    public Vector3 _offset;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = (_followObject.transform.position + _offset);
+	}
+}
